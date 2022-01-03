cae6150f10d868c83bdfdccb72ed1ab7e099157f
coltontcrowe <coltontcrowe@gmail.com>
2022-01-02 22:19:15 -0800

    Add astro rederers to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 export default {
+  renderers: ['@astrojs/renderer-preact'],
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
-  dist: 'public',       // When running `astro build`, path to final static output
   public: 'static',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+  dist: 'public',       // When running `astro build`, path to final static output
   buildOptions: {
     site: 'https://oneirocosm.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
     sitemap: true,         // Generate sitemap (set to "false" to disable)
@@ -11,5 +12,4 @@ export default {
     // hostname: 'localhost',  // The hostname to run the dev server on.
     // port: 3000,             // The port to run the dev server on.
   },
-  renderers: [],
 };
