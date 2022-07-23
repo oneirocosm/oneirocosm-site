58f2803b386ead05d8d769173ed6e4dadcb11db5
coltontcrowe <coltontcrowe@gmail.com>
2022-07-22 22:22:14 -0700

    Update packages to latest versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
-export default {
-  renderers: ['@astrojs/renderer-preact'],
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/preact';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), sitemap(), tailwind()],
   // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
   // pages: './src/pages', // Path to Astro components, pages, and data
-  public: 'static',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  dist: 'public',       // When running `astro build`, path to final static output
-  buildOptions: {
-    site: 'https://oneirocosm.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true,         // Generate sitemap (set to "false" to disable)
-  },
-  devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
-  },
-};
+  publicDir: 'static',
+  // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+  root: 'public',
+  // When running `astro build`, path to final static output
+  site: 'https://oneirocosm.com' // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+
+});
