9a4432dc12601f800d4f124afd27d8bd3b7cd2fb
coltontcrowe <coltontcrowe@gmail.com>
2022-07-22 22:33:51 -0700

    Change root dir to default

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,9 @@ export default defineConfig({
   // pages: './src/pages', // Path to Astro components, pages, and data
   publicDir: 'static',
   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  root: 'public',
+  //root: 'public',
   // When running `astro build`, path to final static output
-  site: 'https://oneirocosm.com' // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  site: 'https://oneirocosm.com', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  sitemap: true,
 
 });
