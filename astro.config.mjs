5fe3119f27bad4419368588836d893e25275f895
Sylvia Crowe <software@oneirocosm.com>
2026-03-16 07:24:25 -0700

    fix: correctly highlight section in navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://oneirocosm.com",
   integrations: [mdx(), sitemap()],
+  trailingSlash: "always",
   markdown: {
     shikiConfig: {
       themes: {
